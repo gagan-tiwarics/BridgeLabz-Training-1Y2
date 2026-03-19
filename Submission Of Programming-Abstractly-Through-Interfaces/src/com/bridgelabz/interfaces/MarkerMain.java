72db346dd7b37c389eb5fb9f1029c98c0d9f4b84
gagan-tiwarics <gagantiwari9548602063@gmail.com>
2026-03-19 13:54:54 +0530

    Delete Submission Of Programming-Abstractly-Through-Interfaces directory

--- Submission Of Programming-Abstractly-Through-Interfaces/src/com/bridgelabz/interfaces/MarkerMain.java
+++
@@ -1,8 +0,0 @@
-
-package com.bridgelabz.interfaces;
-public class MarkerMain{
-public static void main(String[] args){
-UserData u=new UserData();
-if(u instanceof Sensitive)
-System.out.println("Encrypting sensitive data");
-}}
--- Submission Of Programming-Abstractly-Through-Interfaces/src/com/bridgelabz/interfaces/UserData.java
+++
@@ -1,5 +0,0 @@
-
-package com.bridgelabz.interfaces;
-class UserData implements Sensitive{
-String name="Dev";
-}
