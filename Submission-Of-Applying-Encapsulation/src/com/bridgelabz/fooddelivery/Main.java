ffa76113d787d19b11f824f725b1cd6c3423afab
gagan-tiwarics <gagantiwari9548602063@gmail.com>
2026-03-19 13:55:20 +0530

    Delete Submission-Of-Applying-Encapsulation directory

--- Submission-Of-Applying-Encapsulation/src/com/bridgelabz/fooddelivery/Main.java
+++
@@ -1,7 +0,0 @@
-
-package com.bridgelabz.fooddelivery;
-public class Main{
-public static void main(String[] args){
-System.out.println("fooddelivery system running");
-}
-}
--- Submission-Of-Applying-Encapsulation/src/com/bridgelabz/vehiclerental/Main.java
+++
@@ -1,7 +0,0 @@
-
-package com.bridgelabz.vehiclerental;
-public class Main{
-public static void main(String[] args){
-System.out.println("vehiclerental system running");
-}
-}
