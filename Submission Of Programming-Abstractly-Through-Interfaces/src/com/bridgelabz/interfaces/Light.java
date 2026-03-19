d940dcd0632a9d773516bf628a2300380cda69c7
gagan-tiwarics <gagantiwari9548602063@gmail.com>
2026-03-19 14:04:09 +0530

    Delete Submission Of Programming-Abstractly-Through-Interfaces directory

--- Submission Of Programming-Abstractly-Through-Interfaces/src/com/bridgelabz/interfaces/Light.java
+++
@@ -1,6 +0,0 @@
-
-package com.bridgelabz.interfaces;
-class Light implements SmartDevice{
-public void turnOn(){System.out.println("Light ON");}
-public void turnOff(){System.out.println("Light OFF");}
-}
