34e78316d9a3f81ffcd3ac30bc1dd155c25b3380
gagan-tiwarics <gagantiwari9548602063@gmail.com>
2026-03-19 14:02:43 +0530

    Delete Submission-Of-Applying-Encapsulation directory

--- Submission-Of-Applying-Encapsulation/src/com/bridgelabz/employee/FullTimeEmployee.java
+++
@@ -1,6 +0,0 @@
-
-package com.bridgelabz.employee;
-public class FullTimeEmployee extends Employee{
-public FullTimeEmployee(int id,String name,double salary){super(id,name,salary);}
-public double calculateSalary(){return salary;}
-}
