e5f4c776a4ce5d548a1ebaab6c9cb75016b39dc4
gagan-tiwarics <gagantiwari9548602063@gmail.com>
2026-03-19 14:04:26 +0530

    Delete Submission-Of-Applying-Encapsulation directory

--- Submission-Of-Applying-Encapsulation/src/com/bridgelabz/banking/BankMain.java
+++
@@ -1,9 +0,0 @@
-
-package com.bridgelabz.banking;
-public class BankMain{
-public static void main(String[] args){
-BankAccount b=new SavingsAccount(10000);
-b.deposit(2000);
-b.display();
-System.out.println(b.calculateInterest());
-}}
--- Submission-Of-Applying-Encapsulation/src/com/bridgelabz/ridehailing/Main.java
+++
@@ -1,7 +0,0 @@
-
-package com.bridgelabz.ridehailing;
-public class Main{
-public static void main(String[] args){
-System.out.println("ridehailing system running");
-}
-}
